dd9f65de3728cb046cb41f8d2400a2a5c1b44854
Julian Berman <Julian@GrayVines.com>
2019-12-30 19:29:05 +0000

    And now remove the big ARW from the dist.
    Have it locatable in the repo, or via an env var.

--- libraw_cffi/tests/__init__.py
+++ libraw_cffi/tests/__init__.py
@@ -1,7 +1,18 @@
+import os
+
 try:
     from pathlib2 import Path
 except ImportError:
     from pathlib import Path
 
-RADIOHEAD = Path(__file__).parent / "_DSC2164.ARW"
+if "RADIOHEAD" in os.environ:
+    RADIOHEAD = Path(os.environ["RADIOHEAD"])
+else:
+    RADIOHEAD = Path(__file__).parent / "_DSC2164.ARW"
+    if not RADIOHEAD.exists():
+        raise RuntimeError(
+            "Radiohead exists, I promise, but the test suite can't find them. "
+            "Run the suite from alongsie a git checkout, or set the RADIOHEAD "
+            "environment variable to the path of the _DSC2164.ARW file."
+        )
 RADIOHEAD_SIZE = 5216, 3464
